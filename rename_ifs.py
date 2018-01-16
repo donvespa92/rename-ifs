af593a1ee020a802b8d4f5175332d90a8b43e853
Bence Darázs <bence.darazs@econ.local>
2018-01-16 16:23:25 +0100

    Gui is implemented, no methods

--- rename_ifs.py
+++ rename_ifs.py
@@ -24,10 +24,21 @@ class define_tags(tk.Frame):
         self.frame_tags = tk.Frame(self.master)
         
         self.label_tagsolid = tk.Label(self.frame_tags,text='Solid domain tag')
-        self.entry_tagsolid = tk.Entry(self.frame_tags,width=50)
+        self.entry_tagsolid = tk.Entry(self.frame_tags,width=30)
         self.label_tagfluid = tk.Label(self.frame_tags,text='Fluid domain tag')
-        self.entry_tagfluid = tk.Entry(self.frame_tags,width=50)
+        self.entry_tagfluid = tk.Entry(self.frame_tags,width=30)
         
+        self.label_tagssif = tk.Label(self.frame_tags,text='Solid Solid interface tag')
+        self.entry_tagssif = tk.Entry(self.frame_tags,width=30)
+        self.label_tagffif = tk.Label(self.frame_tags,text='Fluid Fluid interface tag')
+        self.entry_tagffif = tk.Entry(self.frame_tags,width=30)
+        self.label_tagfsif = tk.Label(self.frame_tags,text='Fluid Solid interface tag')
+        self.entry_tagfsif = tk.Entry(self.frame_tags,width=30)
+        
+        self.button_write = tk.Button(
+                self.frame_tags,
+                text='Write',
+                height=2)
         
 
 class AppRenameIfs(tk.Frame):
@@ -35,8 +46,33 @@ class AppRenameIfs(tk.Frame):
         tk.Frame.__init__(self,master)
         self.master = master
         self.master.option_add("*Font", "Arial 12")
+        self.mainframe = tk.Frame(self.master)
+        self.mainframe.pack(fill='both',expand=1)
         
+        
+        self.fp = filepaths(self)
+        self.fp.frame_filepaths.pack(fill='both',expand=1,padx=5,pady=5)
+        self.fp.label_inputfile.grid(row=1,column=1,sticky='W')
+        self.fp.entry_inputfile.grid(row=2,column=1,sticky='NSEW')
+        self.fp.label_outputfile.grid(row=3,column=1,sticky='W')
+        self.fp.entry_outputfile.grid(row=4,column=1,sticky='NSEW')
+        self.fp.button_selectinput.grid(row=2,column=2,sticky='NSEW')
+        self.fp.button_selectoutput.grid(row=4,column=2,sticky='NSEW')
 
+        self.tags = define_tags(self)
+        self.tags.frame_tags.pack(fill='both',expand=1,padx=5,pady=5)
+        self.tags.label_tagsolid.grid(row=1,column=1,sticky='E')
+        self.tags.entry_tagsolid.grid(row=1,column=2,sticky='NSEW',padx=5,pady=5)
+        self.tags.label_tagfluid.grid(row=2,column=1,sticky='E')
+        self.tags.entry_tagfluid.grid(row=2,column=2,sticky='NSEW',padx=5,pady=5)
+        self.tags.label_tagssif.grid(row=3,column=1,sticky='E')
+        self.tags.entry_tagssif.grid(row=3,column=2,sticky='NSEW',padx=5,pady=5)
+        self.tags.label_tagffif.grid(row=4,column=1,sticky='E')
+        self.tags.entry_tagffif.grid(row=4,column=2,sticky='NSEW',padx=5,pady=5)
+        self.tags.label_tagfsif.grid(row=5,column=1,sticky='E')
+        self.tags.entry_tagfsif.grid(row=5,column=2,sticky='NSEW',padx=5,pady=5)
+        
+        self.tags.button_write.grid(row=6,column=1,columnspan=2,sticky='NSEW')
 
 if __name__ == '__main__':
     root = tk.Tk()
