9cff8946a9b20e6b72608a6be48d3f311b0e6d2d
Bence Darázs <bence.darazs@econ.local>
2018-01-16 17:09:16 +0100

    Gui ok, some methods too

--- rename_ifs.py
+++ rename_ifs.py
@@ -58,6 +58,8 @@ class AppRenameIfs(tk.Frame):
         self.fp.entry_outputfile.grid(row=4,column=1,sticky='NSEW')
         self.fp.button_selectinput.grid(row=2,column=2,sticky='NSEW')
         self.fp.button_selectoutput.grid(row=4,column=2,sticky='NSEW')
+        self.fp.button_selectinput.config(command=self.cmd_selectinput)
+        self.fp.button_selectoutput.config(command=self.cmd_selectoutput)
 
         self.tags = define_tags(self)
         self.tags.frame_tags.pack(fill='both',expand=1,padx=5,pady=5)
@@ -73,6 +75,96 @@ class AppRenameIfs(tk.Frame):
         self.tags.entry_tagfsif.grid(row=5,column=2,sticky='NSEW',padx=5,pady=5)
         
         self.tags.button_write.grid(row=6,column=1,columnspan=2,sticky='NSEW')
+        self.tags.button.config(command=self.cmd_write)
+        
+        
+    def cmd_selectinput(self):
+        selected = tk.filedialog.askopenfilename(
+                title='Choose the .ccl file with default domains',
+                filetypes=(("CFX Command file", "*.ccl"),("All files", "*.*")))
+        
+        if selected:
+            self.inputfile_fullpath = selected
+            self.inputfile_name = os.path.basename(self.inputfile_fullpath)
+            self.inputfile_dir_name = os.path.dirname(self.inputfile_fullpath)
+            self.inputfile_ext = os.path.splitext(self.inputfile_fullpath)[1]
+            
+            self.fp.entry_inputfile.delete(0,'end')
+            self.fp.entry_inputfile.insert(0,selected)
+        else:
+            return
+    
+    def cmd_selectoutput(self):
+        selected = filedialog.asksaveasfilename(parent=self.master,
+                                    initialdir=self.inputfile_dir_name,
+                                    title="Selet a file for export",
+                                    filetypes=[('All files', '.*')]) 
+    
+        if selected:
+            self.outpufile_fullpath = selected
+            self.fp.entry_outputfile.delete(0,'end')
+            self.fp.entry_outputfile.insert(0,selected)
+        else:
+            return
+    
+    
+    def read_data_from_ccl(self):
+        self.inputdata = []
+        with open(self.inputfile_fullpath) as f:
+            for line in f:
+                self.inputdata.append(line.strip())
+        
+    def get_if_names(self):
+        self.default_if_names = []
+        for line in self.inputdata:
+            if 'DOMAIN INTERFACE:' in line:
+                self.default_if_names.append(line.split(' : ')[1])
+    
+    def get_dom_names(self):
+        self.dom_names = []
+        for line in self.inputdata:
+            if 'DOMAIN:' in line:
+                self.dom_names.append(line.split(' : ')[1])
+    
+    def cmd_write(self):
+        # do check!!!
+        
+        self.read_data_from_ccl()
+        self.get_if_names()
+        self.get_dom_names()
+        
+        pass
+    
+    def check_entries(self):
+        if not os.path.isfile(self.fp.entry_inputfile.get()):
+            print ('Path of inputfile is invalid!')
+            return False
+        if not os.path.isdir(os.path.dirname(self.fp.entry_outputfile.get())):
+            self.check = False
+            print ('Path of outputfile is invalid!')
+            return False
+        if not self.tags.entry_tagfluid.get():
+            self.check = False
+            print ('Define tag for fluid domains!')
+            return False
+        if not self.tags.entry_tagsolid.get():
+            self.check = False
+            print ('Define tag for solid domains!')
+            return False
+        if not self.tags.entry_tagssif.get():
+            self.check = False
+            print ('Define tag for solid solid interfaces!')
+            return False
+        if not self.tags.entry_tagffif.get():
+            self.check = False
+            print ('Define tag for fluid fluid interfaces!')
+            return False
+        if not self.tags.entry_tagfsif.get():
+            self.check = False
+            print ('Define tag for fluid solid interfaces!')
+            return False
+        else:
+            return True
 
 if __name__ == '__main__':
     root = tk.Tk()
